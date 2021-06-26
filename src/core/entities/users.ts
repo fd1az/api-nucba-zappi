e3d03da8af8ac4744279f0d7934aa9519c7f71e6
facu diaz <fdiaz.dev@gmail.com>
2021-06-26 10:09:23 -0300

    agregamos entities y dtos

---
+++ src/core/entities/users.ts
@@ -0,0 +1,19 @@
+export interface UserAddress {
+  id: number;
+  street: string;
+  number: string;
+  zipCode: string;
+  city: string;
+  createdAt: Date;
+  updatedAt: Date;
+  userId: number;
+}
+
+export interface User {
+  id: number;
+  email: string;
+  name: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
