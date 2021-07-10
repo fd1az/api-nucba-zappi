d5ed267295fd008386650f35655c27f9acb5409a
facu diaz <fdiaz.dev@gmail.com>
2021-07-10 10:38:12 -0300

    cambiamos el manejo de respuestas, agregamos middleware para validar los request

--- src/data/category.datasource.ts
+++ src/data/category.datasource.ts
@@ -1,17 +1,31 @@
 import { Category } from '../core/entities/category';
 import CategoryRepository from '../core/respositories/category.repository';
+import { Result } from '../core/types/response';
 import prisma from '../config/db';
+import { BadRequestError } from '../errors/bad-request-error';
+import { ServerError } from '../errors/server-error';
 
 export default class CategoryDataSource implements CategoryRepository {
-  public async getCategory(): Promise<Category[]> {
-    const category = await prisma.category.findMany();
-    return category;
-  }
-  public async createCategory(categoryName: string): Promise<Category> {
-    const category = await prisma.category.create({
-      data: { category: categoryName },
-    });
+  public async getCategory(): Promise<Result<Category[]>> {
+    try {
+      const categories = await prisma.category.findMany();
 
-    return category;
+      return { success: true, result: categories };
+    } catch (error) {
+      let err = new ServerError('Algo salio mal al traer categorias!');
+      return { success: false, err };
+    }
+  }
+  public async createCategory(categoryName: string): Promise<Result<Category>> {
+    try {
+      const category = await prisma.category.create({
+        data: { category: categoryName },
+      });
+      return { success: true, result: category };
+    } catch (error) {
+      //El error real, deberiamos loggearlo a la consola o un file .log (para info nuestra)
+      let err = new BadRequestError('Error al crear Categoria!');
+      return { success: false, err };
+    }
   }
 }
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { signin, login } from '../controllers/auth.controller';
 import { body } from 'express-validator';
-
+import { validateRequest } from '../middlewares/validate-request';
 const router = Router();
 
 router.post(
@@ -9,12 +9,14 @@ router.post(
   body('email').isEmail().withMessage('Error en Email'),
   body('name').trim().notEmpty().withMessage('Nombre obligatorio'),
   body('password').trim().notEmpty().withMessage('Password obligatorio'),
+  validateRequest,
   signin
 );
 router.post(
   '/login',
   body('email').isEmail().withMessage('Error en Email'),
   body('password').trim().notEmpty().withMessage('Password obligatorio'),
+  validateRequest,
   login
 );
 
